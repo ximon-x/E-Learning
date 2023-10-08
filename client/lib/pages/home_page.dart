cf9da761b34e0d9948e943ff1236406cbdfceb68
Simon Samuel <salviensky@gmail.com>
2023-10-08 06:43:45 +0100

    chore: added bottom selection bar

--- client/lib/pages/home_page.dart
+++ client/lib/pages/home_page.dart
@@ -3,19 +3,55 @@ import 'package:flutter/material.dart';
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  static const String routeName = '/home';
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
+  bool signedIn = true;
+
+  @override
+  initState() {
+    signedIn
+        // ignore: avoid_print
+        ? print("Signed in")
+        : Navigator.pushReplacementNamed(context, "/login");
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Demo Home Page'),
+      appBar: AppBar(
+          title: const Text('i-learn'),
+          leading: const Icon(Icons.person),
+          centerTitle: true,
+          titleTextStyle: const TextStyle(fontWeight: FontWeight.bold)),
+      body: const Center(),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(
+          icon: const Icon(Icons.dashboard),
+          label: "Dashboard",
+          backgroundColor: Theme.of(context).primaryColor,
+          tooltip: "Dashboard",
         ),
-        body: const Center());
+        BottomNavigationBarItem(
+          icon: const Icon(Icons.quiz),
+          label: "Take Quiz",
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+        BottomNavigationBarItem(
+          icon: const Icon(Icons.work_history),
+          label: "Review Progress",
+          backgroundColor: Theme.of(context).primaryColor,
+        ),
+        BottomNavigationBarItem(
+          icon: const Icon(Icons.info),
+          label: "About the Project",
+          backgroundColor: Theme.of(context).primaryColor,
+        )
+      ]),
+    );
   }
 }
