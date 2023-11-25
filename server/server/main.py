93d2c18239ba5111be7c06151fd49ddf5ac4f04f
Simon Samuel <salviensky@gmail.com>
2023-11-25 05:11:18 +0100

    chore: writing server

--- server/server/main.py
+++ server/server/main.py
@@ -8,5 +8,20 @@ def home():
     return "Hello, World!"
 
 
+@app.route("/get-best-hour", methods=["POST"])
+def get_best_hour():
+    return "Best Hour!"
+
+
+@app.route("/get-best-subject", methods=["POST"])
+def get_best_subject():
+    return "Best Subject!"
+
+
+@app.route("/linear_regress", methods=["POST"])
+def linear_regression():
+    return "Linear Regress!"
+
+
 if __name__ == "__main__":
     app.run(debug=True)
